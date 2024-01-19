f93921eb37b72207905c905a3791d5babb26c8e2
Shin You <shin_you@tomofun.com>
2024-01-19 21:54:59 +0800

    feat(review): add review page

--- src/app.py
+++ src/app.py
@@ -1,25 +1,31 @@
 # app.py
-from flask import Flask, render_template,request
+from utils.question import generate_question
+from flask import Flask, render_template, request
 import json
 from os import listdir
 import sys
 sys.path.append('src/')
-from utils.question import generate_question 
 
-app = Flask(__name__,template_folder='templates')
+app = Flask(__name__, template_folder='templates')
 
 
-@app.route("/",methods=['GET'])
+@app.route("/", methods=['GET'])
 def index():
     csv_list = listdir("src/data")
-    
-    return render_template("index.html", csv_list=sorted(csv_list,key=len))
 
-@app.route("/exam/<selectFileName>",methods=['GET'])
+    return render_template("index.html", csv_list=sorted(csv_list, key=len))
+
+
+@app.route("/exam/<selectFileName>", methods=['GET'])
 def exam(selectFileName):
     data = generate_question(selectFileName)
-    print(data[1])
     return render_template("exam.html", selectFileName=selectFileName, questionList=data[0], answerList=data[1])
+
+
+@app.route("/review/<selectFileName>", methods=['GET'])
+def review(selectFileName):
+    data = generate_question(selectFileName)
+    return render_template("review.html", selectFileName=selectFileName, questionList=data[0], answerList=data[1])
 
 
 if __name__ == "__main__":
